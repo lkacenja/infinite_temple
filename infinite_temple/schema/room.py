9998a7431795cb62c46f5f7bac406c4316ef82df
Leonard Kacenjar <leonardkacenjar@Unknown.domain>
2025-11-29 13:51:22 -0700

    Add the missing room attributes.

--- infinite_temple/schema/room.py
+++ infinite_temple/schema/room.py
@@ -12,6 +12,10 @@ class Segment(BaseModel):
 
 class Room(BaseModel):
     id: int = Field(description="Unique identifier for the room in the series")
+    entry_edge: Literal["top", "bottom", "left", "right"] = Field(description="Which edge the passage starts from")
+    exit_edge: Literal["top", "bottom", "left", "right"] = Field(description="Which edge the passage ends at")
+    entry_point: Point = Field(description="Exact coordinate where passage starts on the entry edge")
+    exit_point: Point  = Field(description="Exact coordinate where passage ends on the exit edge")
     walls: List[Segment] = Field(min_length=10, max_length=100, description="Walls making up the passage")
 
 class MapSequence(BaseModel):
